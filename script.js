2f285349b6f108b41428981c62a6880f111c7816
Dave Berzack <dberzack>
2015-12-16 16:57:34 -0500

    s

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
 var media = document.getElementById('media');
 
+var isPlaying = false;
+
 $('#play').bind('click', function() {
-  $("#media").attr("src", "test.mp3" );
-  media.currentTime = $("#start").val();
-  media.play(); 
+  if (isPlaying) doStop();
+  else doPlay();
 });
-$('#stop').bind('click', function() {
+
+function doStop() {
+  isPlaying=false;
+  $("#play").attr("src", "play.png");
   $("#media").attr("src", "" );
-});
+  media.currentTime = 0;
+  clearInterval(playInterval);
+  $("#current").val( "0.0" );
+}
+
+function doPlay(){
+  isPlaying=true;
+  $("#play").attr("src", "stop.png");
+  $("#media").attr("src", "test.mp3" );
+  media.currentTime = $("#current").val();
+  media.play();
+  
+  playInterval = setInterval(function(){
+    $("#current").val( media.currentTime.toFixed(1) );
+  }, 100); 
+}
 
 $('.nudge').bind('click', function() {
   media.currentTime += 1*$(this).attr("data-amount");
 });
 
-setInterval(function(){
-  $("#current").val( Math.round(media.currentTime*10)/10 );
-}, 100);
+var playInterval = 0;
+
